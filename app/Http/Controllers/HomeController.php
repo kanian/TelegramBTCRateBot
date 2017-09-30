7e48988a1bfb93603f4745881067b26ef183022d
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-09-30 18:35:16 +0200

    adding Telegram Laravel Integration

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Telegram\Bot\Api;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $telegram = new Api('TELEGRAM_BOT_TOKEN');
+        $response = $telegram->getMe();
+
+        $botId = $response->getId();
+        $firstName = $response->getFirstName();
+        $username = $response->getUsername();
+        return view('home')->with(array('botId'=>$botId, '$firstName' => $firstName));
+    }
+}
