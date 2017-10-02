3e4922d6c49142f42495a7c16b64ecbe949f85ff
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-03 01:57:31 +0200

    enforecing remove webhook

--- app/Adapters/TelegramWebHookAdapter.php
+++ app/Adapters/TelegramWebHookAdapter.php
@@ -15,15 +15,15 @@ class TelegramWebHookAdapter {
     
     public function __construct() {
         $telegram = app('App\Adapters\TelegramBotApiAdapter')->Instance();
-        //$telegram->removeWebhook();
+        $telegram->removeWebhook();
         // We are supplying a self-signed-certificate
-        if(!TelegramWebHookAdapter::$webhook_is_set){
+        /*if(!TelegramWebHookAdapter::$webhook_is_set){
           $response = $telegram->setWebhook([
           'url' => 'https://'.HTTP_HOST.WEBHOOK_ROUTE.'/webhook',
           'certificate' => '/etc/ssl/crt/btcratebot.crt'
         ]);
           TelegramWebHookAdapter::$webhook_is_set= true;
-        } 
+        } */
         
     }
     
