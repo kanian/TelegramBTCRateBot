c2ae8c68bf2175dc7d4f6ae6aaf58dd218c4b411
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-03 12:19:17 +0200

    debugging

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
         $telegram = app('App\Adapters\TelegramBotApiAdapter')->Instance();
         $update = $telegram->getUpdates();
         print_r(count($update)) . '\n';
-        print_r($update[14]);
+        print_r($update[0]);
         /*for($i = 0;$i< count($update);$i++){
             
         }*/
