062a57f74893db3edf6e462d3f5021d5e6213f06
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-03 10:56:37 +0200

    gerwebhook updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     {
         //app('App\Adapters\TelegramManualUpdateAdapter')->getUpdates();
         $telegram = app('App\Adapters\TelegramBotApiAdapter')->Instance();
-        print_r($telegram->commandsHandler(true));
+        print_r($telegram->$telegram->getWebhookUpdates());
         return view('home');
     }
 }
