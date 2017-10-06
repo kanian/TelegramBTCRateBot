37e3195c605c35fb667f71b81fb06b3169faaf4d
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-06 14:36:46 +0200

    webhook setup

--- bootstrap/bootstrap_config.php
+++ bootstrap/bootstrap_config.php
@@ -2,5 +2,5 @@
 define('HTTP_HOST', filter_input(INPUT_SERVER, 'HTTP_HOST'));
 define('TELEGRAM_BOT_TOKEN','450338970:AAEw6b2YUpUIUSr72Cf8fuSVeqPq76cbDRo');
 define('API_URL','https://api.telegram.org/bot'.TELEGRAM_BOT_TOKEN.'/');
-define('WEBHOOK_ROUTE','/'. str_replace ( ':' ,  '_',  TELEGRAM_BOT_TOKEN) . '/webhook');
+define('WEBHOOK_ROUTE','/'. /*str_replace ( ':' ,  '_', */ TELEGRAM_BOT_TOKEN/*)*/ . '/webhook');
 
