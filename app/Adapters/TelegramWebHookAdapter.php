49390c0957054b968594914df532235ebd39f30c
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-06 14:30:51 +0200

    webhook setup

--- app/Adapters/TelegramWebHookAdapter.php
+++ app/Adapters/TelegramWebHookAdapter.php
@@ -12,19 +12,21 @@ use Telegram\Bot\Api;
 class TelegramWebHookAdapter {
     
     public static $webhook_is_set = false;
-    
+    public static $webhook;
     public function __construct() {
         $telegram = app('App\Adapters\TelegramBotApiAdapter')->Instance();
+        TelegramWebHookAdapter::$webhook = 'https://'.HTTP_HOST.WEBHOOK_ROUTE;
         //$telegram->removeWebhook();
         // We are supplying a self-signed-certificate
-        /*if(!TelegramWebHookAdapter::$webhook_is_set){
+        if(!TelegramWebHookAdapter::$webhook_is_set){
           $response = $telegram->setWebhook([
-          'url' => 'https://'.HTTP_HOST.WEBHOOK_ROUTE.'/webhook',
+          'url' => TelegramWebHookAdapter::$webhook,
           'certificate' => '/etc/ssl/crt/35_176_171_82.crt'
         ]);
           TelegramWebHookAdapter::$webhook_is_set= true;
-        } */
-        print_r('https://'.HTTP_HOST.WEBHOOK_ROUTE);
+          print_r(TelegramWebHookAdapter::$webhook);
+        } 
+       
     }
     
     public function removeWebhook(){
