6af7926531d0e0dfb5da8f973e2abaa54cc7d698
Patrick Assoa Adou <patrick.assoa.adou@gmail.com>
2017-10-10 23:59:16 +0200

    use route parameter to capture token

--- app/Adapters/TelegramWebHookAdapter.php
+++ app/Adapters/TelegramWebHookAdapter.php
@@ -16,7 +16,6 @@ class TelegramWebHookAdapter {
     public function __construct() {
         $telegram = app('App\Adapters\TelegramBotApiAdapter')->Instance();
         TelegramWebHookAdapter::$webhook = 'https://'.HTTP_HOST.WEBHOOK_ROUTE;
-        dd(TelegramWebHookAdapter::$webhook);
         //$telegram->removeWebhook();
         // We are supplying a self-signed-certificate
         if(!TelegramWebHookAdapter::$webhook_is_set){
